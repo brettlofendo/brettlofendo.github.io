64984546dfd257fa8380af53ac09978491c84ac6
Brett Lofendo <blofendo@gmail.com>
2021-01-27 02:00:25 -0600

    dark mode beta

--- js/main.js
+++ js/main.js
@@ -29,3 +29,59 @@ restapi.onclick = function openRestapi() {
     window.open("https://github.com/brettlofendo/scope", "_blank");
 }
 
+var toggled = false;
+var toggleButton = document.getElementById("toggle_button");
+
+toggleButton.addEventListener("click", function() {
+    if(!toggled) {
+        document.body.classList.toggle("dark_mode"); 
+        document.getElementById("greeting").classList.toggle("dark_mode");
+        document.getElementById("headline").classList.toggle("dark_mode");
+        document.getElementById("contact_body").classList.toggle("dark_mode");
+
+        var titles = document.getElementsByClassName("component_title");
+        for (var i = 0; i < titles.length; i++) {
+            titles[i].classList.toggle("dark_mode");
+        }
+
+        var bodies = document.getElementsByClassName("component_body");
+        for (var i = 0; i < bodies.length; i++) {
+            bodies[i].classList.toggle("dark_mode");
+        }
+
+        var borders = document.getElementsByClassName("grid_item");
+        for (var i = 0; i < borders.length; i++) {
+            borders[i].style.border = "2px solid white";
+        }
+
+        var dividers = document.getElementsByClassName("dotted");
+        for (var i = 0; i < dividers.length; i++) {
+            dividers[i].style.borderBottom = "rgb(20, 20, 20)";
+        }
+        toggled = true;
+    }
+    else {
+        document.body.classList.toggle("dark_mode");
+        document.getElementById("greeting").classList.toggle("dark_mode");
+        document.getElementById("headline").classList.toggle("dark_mode");
+        document.getElementById("contact_body").classList.toggle("dark_mode");
+
+        var titles = document.getElementsByClassName("component_title");
+        for (var i = 0; i < titles.length; i++) {
+            titles[i].classList.toggle("dark_mode");
+        }
+
+        var bodies = document.getElementsByClassName("component_body");
+        for (var i = 0; i < bodies.length; i++) {
+            bodies[i].classList.toggle("dark_mode");
+        }
+
+        var borders = document.getElementsByClassName("grid_item");
+        for (var i = 0; i < borders.length; i++) {
+            borders[i].style.border = "2px solid black";
+        }
+        toggled = false;
+    }
+});
+
+
